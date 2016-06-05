f438d2e97485a43aa6aac481a46e956552e712ca
PUC <maicon.abreu@outlook.com>
2016-06-05 01:35:30 -0300

    Inicio
    Carregamento Inicial

---
+++ app/src/main/java/com/example/encontroibrs/colmeia/Splash.java
@@ -0,0 +1,31 @@
+package com.example.encontroibrs.colmeia;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.Timer;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+
+    }
+}
